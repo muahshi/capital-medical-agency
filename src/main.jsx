460b860466ff94a7ab5cab354f444c771423fcbc
Muahshi Digital <muahshi.mubi@gmail.com>
2026-05-09 23:48:25 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
+```react
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Standard default import
 import './styles/globals.css'
-// Kyunki aapne 'u' small rakha hai, toh path exact yahi hona chahiye
-import { AuthProvider } from './hooks/useAuth' 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>,
 )
 
+```
