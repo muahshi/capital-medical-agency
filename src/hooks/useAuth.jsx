6090333f4f5b7b6370cfee571b01972586d6b01c
Muahshi Digital <muahshi.mubi@gmail.com>
2026-05-09 20:27:13 +0530

    Add files via upload

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,4 +1,3 @@
-```react
 import React, { createContext, useContext, useState, useEffect } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import { initializeApp, getApps } from 'firebase/app';
@@ -49,4 +48,3 @@ export const AuthProvider = ({ children }) => {
 
 export const useAuth = () => useContext(AuthContext);
 
-```
